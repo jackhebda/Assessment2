06ea92c6e731ce8040de52ab4ffcfe9f70a2181d
jack <jackhebda@gmail.com>
2020-12-07 00:41:07 +0100

    almost there

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,12 @@ FROM python:3.8-alpine
 WORKDIR /app
 ADD . /app/
 
-RUN /sbin/apk add --no-cache --virtual .deps gcc musl-dev \
+RUN /sbin/apk add --virtual .deps gcc musl-dev\
  && /usr/local/bin/pip install --no-cache-dir black==19.10b0 \
- && /sbin/apk del --no-cache .deps
+ && /sbin/apk del .deps
 
-RUN apk add --no-cache build-base
+RUN apk add build-base
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5000
-
-RUN python -m uvicorn app:app --reload --log-config logger.conf --log-level debug --port 5000
--- app/db/db.py
+++ app/db/db.py
@@ -3,10 +3,12 @@ from __future__ import absolute_import
 import abc
 import asyncio
 import logging
+import csv
 from functools import wraps
 from typing import Type
 
 import sqlalchemy
+from sqlalchemy import *
 from databases import Database
 from starlette.requests import Request
 
@@ -14,6 +16,29 @@ metadata = sqlalchemy.MetaData()
 
 
 def get_db(request: Request):
+    engine = create_engine(DB_DSN, echo = False)
+    conn = engine.connect()
+
+    with open('/app/data/zipcodes.csv', newline='') as f:
+            zipcode_dict = {}
+            reader = csv.reader(f)
+            for row in reader:
+                if len(row[0]) == 4:
+                    zipcode_dict[int(row[0])] = row[1]
+                else:
+                    for zip in range(int(row[0][:4]), int(row[0][7:]) + 1):
+                        zipcode_dict[zip] = row[1]
+
+    metadata = MetaData()
+
+    riskfactor = Table('riskfactor', metadata,
+        Column('zipcode', Integer, primary_key=True),
+        Column('risk_factor', String))                
+                    
+    metadata.create_all(engine)
+
+    for k, v in zipcode_dict.items():
+        conn.execute(insert(riskfactor).values(zipcode = k, risk_factor = v))
     return request.app.db.db
 
 
